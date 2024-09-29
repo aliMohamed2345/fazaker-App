c7432b05674823ee6e920f0909f3567fce1ddad5
ali <eloymohamedalimansor@gmail.com>
2024-09-29 16:45:39 +0300

    the project has been finished

--- src/app/Hadith/[HadithId]/[SearchId]/page.tsx
+++ src/app/Hadith/[HadithId]/[SearchId]/page.tsx
@@ -5,7 +5,8 @@ import { HadithProps } from "../page";
 import HadithContainer from "@/app/components/Hadith/HadithContainer";
 import SearchHadithComponent from "@/app/components/Hadith/SearchHadith";
 import HadithLoading from "@/app/components/Hadith/HadithLoading";
-
+import { useDispatch } from "react-redux";
+import { SetHadithContent } from "@/app/redux/Slices/HadithContainerSlice";
 interface searchParamsProps {
     searchParams: {
         HadithNum: number;
@@ -22,6 +23,7 @@ let InitialHadith: HadithProps = {
 const SearchHadith = ({ searchParams }: searchParamsProps) => {
     let [Hadith, SetHadith] = useState<HadithProps>(InitialHadith);
     let [IsLoading, SetIsLoading] = useState<boolean>(true)
+    let dispatch = useDispatch();
     let Api = `https://api.hadith.gading.dev/books/${searchParams.Hadith}/${searchParams.HadithNum}`
     useEffect(() => {
         fetch(Api)
@@ -30,13 +32,13 @@ const SearchHadith = ({ searchParams }: searchParamsProps) => {
                 SetIsLoading(false)
             });
     }, [])
-    let { number, arab } = Hadith;
+    // let { number, arab } = Hadith;
+    dispatch(SetHadithContent([Hadith]))
     return (<>
         <div className="container">
             <h1 className="mt-5 mb-5 text-center">{searchParams.HadithBook}</h1>
             <SearchHadithComponent />
-            {IsLoading ? <HadithLoading /> :
-                <HadithContainer NumberOfHadith={number} HadithContent={arab} />
+            {IsLoading ? <HadithLoading /> : <HadithContainer KeyVal={-1} />
             }
         </div>
     </>
--- src/app/components/Hadith/HadithContainer.tsx
+++ src/app/components/Hadith/HadithContainer.tsx
@@ -5,10 +5,9 @@ interface HadithContainerProps {
     KeyVal: number; // Only KeyVal is passed
 }
 
-const HadithContainer = ({ KeyVal }: HadithContainerProps) => {
+const HadithContainer = ({ KeyVal = -1 }: HadithContainerProps) => {
     // Get the specific Hadith data using the index KeyVal
-    const Hadiths = useSelector((state: RootState) => state.HadithContainerSlice.KeyVal[KeyVal]);
-    const { number, arab } = Hadiths;
+    const { number, arab } = useSelector((state: RootState) => state.HadithContainerSlice.KeyVal[KeyVal === -1 ? 0 : KeyVal])
     return (
         <div key={KeyVal} className="hadith p-3 rounded-4 d-flex flex-column">
             <p className="text-center hadith-number text-center d-block p-1 rounded-circle m-auto mb-3">
